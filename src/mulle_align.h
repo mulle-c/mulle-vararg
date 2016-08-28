ee017732cae2ac11d3e6944d596450cffb9be952
Nat! <nat@codeon.de>
2016-08-28 18:47:32 +0200

    fix for windows

--- src/mulle_align.h
+++ src/mulle_align.h
@@ -9,12 +9,28 @@
 #ifndef mulle_align_h__
 #define mulle_align_h__
 
-#include <stdalign.h>
 #include <stdint.h>
 #include <stddef.h>
 
+// ugliness creeps in due to windows
+#ifdef _WIN32
+# define alignof( x)  __alignof( x)
+#else
+# include <stdalign.h>
+#endif
 
-__attribute(( const))
+#if ! defined( __clang__) && ! defined( __GNUC__)
+# ifndef MULLE_CONST_VALUE_RETURN
+#  define MULLE_CONST_VALUE_RETURN   __attribute__(( const))
+# endif
+#else
+# ifndef MULLE_CONST_VALUE_RETURN
+#  define MULLE_CONST_VALUE_RETURN
+# endif
+#endif
+
+
+MULLE_CONST_VALUE_RETURN
 static inline uintptr_t   mulle_align( uintptr_t p, unsigned int alignment)
 {
    intptr_t   misalignment;
@@ -29,7 +45,7 @@ static inline uintptr_t   mulle_align( uintptr_t p, unsigned int alignment)
 }
 
 
-__attribute(( const))
+MULLE_CONST_VALUE_RETURN
 static inline void   *mulle_align_pointer( void *p, unsigned int alignment)
 {
    return( (void *) mulle_align( (uintptr_t) p, alignment));
