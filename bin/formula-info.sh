371fe59514715b4df096a346a2adf52fc4aecfbd
Nat! <nat@mulle-kybernetik.com>
2017-06-14 15:37:44 +0200

    Use templates to create .md files where necessary

--- bin/formula-info.sh
+++ bin/formula-info.sh
@@ -1,3 +1,3 @@
-PROJECT="MulleVararg"   # your project name, requires camel-case
+PROJECT="mulle-vararg"   # your project name, requires camel-case
 DESC="Access variable arguments in struct layout fashion"
 DEPENDENCIES='${DEPENDENCY_TAP}mulle-c11'
